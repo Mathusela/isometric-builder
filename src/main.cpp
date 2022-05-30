38d7cf1a8137ef42a8c46e06c5618ef011e01cd5
Mathusela <46897785+Mathusela@users.noreply.github.com>
2022-05-30 12:03:52 +0100

    Initialize window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
+const unsigned int WIDTH = 300, HEIGHT = 300;
+const char* TITLE = "Isometric builder";
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	// Initialize GLFW and create a window
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	auto window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, nullptr, nullptr);
+
+	glfwMakeContextCurrent(window);
+
+	// Bind OpenGL addresses
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	
+	// Set OpenGL viewport size
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	// Game loop
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
 	return 0;
 }
